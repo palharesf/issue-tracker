949583952cd887e352b2effffa27c8cfb4ab3f01
Fernando <palhares.f@gmail.com>
2024-12-27 17:27:34 -0300

    POST requests creating issues implemented

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,26 @@
 "use strict";
 
+const issues = new Map();
+
+const idGenerator = (() => {
+  let _id = 0;
+  return () => {
+    _id++;
+    return _id;
+  };
+})();
+
 module.exports = function (app) {
   app
     .route("/api/issues/:project")
 
     .get(function (req, res) {
       let project = req.params.project;
+      let issuesForProject = Array.from(issues.values()).filter(
+        (issue) => issue.project === project
+      );
+
+      res.json(issuesForProject);
 
       // 5. You can send a GET request to /api/issues/{projectname} for an array of all issues for that specific projectname, with all the fields present for each issue.
       // 6. You can send a GET request to /api/issues/{projectname} and filter the request by also passing along any field and value as a URL query (ie. /api/issues/{project}?open=false). You can pass one or more field/value pairs at once.
@@ -14,7 +29,37 @@ module.exports = function (app) {
     .post(function (req, res) {
       let project = req.params.project;
 
-      // 2. You can send a POST request to /api/issues/{projectname} with form data containing the required fields issue_title, issue_text, created_by, and optionally assigned_to and status_text
+      let issue_title = req.query.issue_title;
+      let issue_text = req.query.issue_text;
+      let created_by = req.query.created_by;
+      let assigned_to = req.query.assigned_to; //Optional
+      let status_text = req.query.status_text; //Optional
+      let created_on = new Date();
+      let updated_on = new Date();
+      let open = true;
+
+      if (issue_title == "" || issue_text == "" || created_by == "") {
+        res.json({ error: "required field(s) missing" });
+      } else {
+        const newIssue = {
+          _id: idGenerator().toString(),
+          issue_title: req.body.issue_title,
+          issue_text: req.body.issue_text,
+          created_by: req.body.created_by,
+          assigned_to: req.body.assigned_to,
+          status_text: req.body.status_text,
+          created_on: created_on,
+          updated_on: updated_on,
+          open: open,
+          project: project,
+        };
+
+        issues.set(newIssue._id, newIssue);
+        console.log(newIssue);
+
+        res.json(issues.get(newIssue._id));
+      }
+
       // 3. The POST request to /api/issues/{projectname} will return the created object, and must include all of the submitted fields. Excluded optional fields will be returned as empty strings. Additionally, include created_on (date/time), updated_on (date/time), open (boolean, true for open - default value, false for closed), and _id.
       // 4. If you send a POST request to /api/issues/{projectname} without the required fields, returned will be the error { error: 'required field(s) missing' }
     })
