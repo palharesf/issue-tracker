173f358bdb8b71200031f91a175efe57cd03116c
Fernando <palhares.f@gmail.com>
2024-12-27 18:11:23 -0300

    Functional GET tests created

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -59,9 +59,43 @@ suite('Functional Tests', function () {
                 done();
             });
     });
-    // View issues on a project: GET request to /api/issues/{project}
-    // View issues on a project with one filter: GET request to /api/issues/{project}
-    // View issues on a project with multiple filters: GET request to /api/issues/{project}
+    
+    test('View issues on a project: GET request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .get('/api/issues/test')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.isArray(res.body);
+                done();
+            });
+    });
+
+    test('View issues on a project with one filter: GET request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .get('/api/issues/test?created_by=Joe')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.isArray(res.body);
+                res.body.forEach(issue => {
+                    assert.equal(issue.created_by, 'Joe');
+                });
+                done();
+            });
+    });
+
+    test('View issues on a project with multiple filters: GET request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .get('/api/issues/test?open=true&assigned_to=Joe')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.isArray(res.body);
+                res.body.forEach(issue => {
+                    assert.equal(issue.open, true);
+                    assert.equal(issue.assigned_to, 'Joe');
+                });
+                done();
+            });
+    });
     // Update one field on an issue: PUT request to /api/issues/{project}
     // Update multiple fields on an issue: PUT request to /api/issues/{project}
     // Update an issue with missing _id: PUT request to /api/issues/{project}
